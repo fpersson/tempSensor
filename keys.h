5227a79264aaec6a0b6243aa01d13f6c149db80e
Fredrik Persson <fpersson.se@gmail.com>
2020-08-06 09:59:33 +0200

    Added: keys for all settings to make it easyer to maintain

---
+++ keys.h
@@ -0,0 +1,37 @@
+/*
+    Copyright (C) 2020 Fredrik Persson <fpersson.se@gmail.com>
+
+    Permission to use, copy, modify, and/or distribute this software
+    for any purpose with or without fee is hereby granted.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+    LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+    OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+    OF THIS SOFTWARE.
+ */
+
+#ifndef TEMPSENSOR_KEYS_H
+#define TEMPSENSOR_KEYS_H
+
+#include <iostream>
+
+namespace core{
+    namespace key {
+        const std::string SENSOR = "SensorSettings.sensor";
+        const std::string SQLITE_DB = "SQLite.db_file";
+        const std::string MQTT_ID = "Mqtt.ID";
+        const std::string MQTT_SERVER = "Mqtt.server";
+        const std::string MQTT_PORT = "Mqtt.port";
+        const std::string MQTT_USERNAME = "Mqtt.username";
+        const std::string MQTT_PASSWORD = "Mqtt.password";
+        const std::string TOPIC1_TOPIC = "Topic1.topic";
+        const std::string TOPIC1_INTERVAL = "Topic1.interval";
+        const std::string TOPIC2_TOPIC = "Topic2.topic";
+        const std::string TOPIC2_INTERVAL = "Topic2.interval";
+    }
+}
+
+#endif //TEMPSENSOR_KEYS_H
--- main.cpp
+++ main.cpp
@@ -27,6 +27,7 @@
 #include "History.h"
 #include "HistoryData.h"
 #include "MqttClient.h"
+#include "keys.h"
 
 int main(int argc, char **argv){
 
@@ -51,26 +52,26 @@ int main(int argc, char **argv){
     utils::IniParser iniParser;
     iniParser.parseFile(ini_file);
 
-    if(iniParser.getValue("SensorSettings.sensor").first){
-        sensor = iniParser.getValue("SensorSettings.sensor").second;
+    if(iniParser.getValue(core::key::SENSOR).first){
+        sensor = iniParser.getValue(core::key::SENSOR).second;
     }else{
         std::cerr << "no sensor defined in " << ini_file << '\n';
     }
 
-    if(iniParser.getValue("Topic1.interval").first){
-        interval = std::stol(iniParser.getValue("Topic1.interval").second);
+    if(iniParser.getValue(core::key::TOPIC1_INTERVAL).first){
+        interval = std::stol(iniParser.getValue(core::key::TOPIC1_INTERVAL).second);
     }else{
         std::cerr << "no interval defined in " << ini_file << " using default 1 minute\n";
     }
 
-    if(iniParser.getValue("SQLite.db_file").first){
-        db_file = iniParser.getValue("SQLite.db_file").second;
+    if(iniParser.getValue(core::key::SQLITE_DB).first){
+        db_file = iniParser.getValue(core::key::SQLITE_DB).second;
     }else{
         std::cerr << "no db_file defined in " << ini_file << '\n';
     }
 
-    if(iniParser.getValue("Topic2.topic").first){
-        history_topic = iniParser.getValue("Topic2.topic").second;
+    if(iniParser.getValue(core::key::TOPIC2_TOPIC).first){
+        history_topic = iniParser.getValue(core::key::TOPIC2_TOPIC).second;
     }else{
         std::cerr << "no Topic2.topic defined in " << ini_file << '\n';
     }
@@ -85,13 +86,13 @@ int main(int argc, char **argv){
 
     TempSensor::MqttSettings mqttSettings;
 
-    mqttSettings.ID=iniParser.getValue("Mqtt.ID").second;
-    mqttSettings.server=iniParser.getValue("Mqtt.server").second;
-    mqttSettings.port=std::stoi(iniParser.getValue("Mqtt.port").second);//15786;
-    mqttSettings.username=iniParser.getValue("Mqtt.username").second;
-    mqttSettings.password=iniParser.getValue("Mqtt.password").second;
+    mqttSettings.ID=iniParser.getValue(core::key::MQTT_ID).second;
+    mqttSettings.server=iniParser.getValue(core::key::MQTT_SERVER).second;
+    mqttSettings.port=std::stoi(iniParser.getValue(core::key::MQTT_PORT).second);//15786;
+    mqttSettings.username=iniParser.getValue(core::key::MQTT_USERNAME).second;
+    mqttSettings.password=iniParser.getValue(core::key::MQTT_PASSWORD).second;
 
-    auto topic = iniParser.getValue("Topic1.topic").second;
+    auto topic = iniParser.getValue(core::key::TOPIC1_TOPIC).second;
 
     std::cout << "Topic:" << topic << std::endl;
 
